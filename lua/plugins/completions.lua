f2df6d0d51e5ed059c62d3004e1b4939bfc15180
kul-sudo <ulyanov.kirill@protonmail.com>
2024-04-04 21:04:59 +0300

    remove docs comments

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,14 +13,11 @@ return {
 
 			cmp.setup({
 				snippet = {
-					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
-					-- completion = cmp.config.window.bordered(),
-					-- documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
