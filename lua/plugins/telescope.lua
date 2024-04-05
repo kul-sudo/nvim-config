0f6c44b7a2c0ba83b85cc7d9124f8824e961ca3a
kul-sudo <ulyanov.kirill@protonmail.com>
2024-04-05 15:10:54 +0300

    save current buffer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,9 @@ return {
 	config = function()
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "sf", function()
-			require("telescope").extensions.file_browser.file_browser()
+			require("telescope").extensions.file_browser.file_browser({
+				path = "%:p:h",
+			})
 		end)
 		vim.keymap.set("n", ";r", builtin.live_grep)
 	end,
