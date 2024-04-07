97f806bb8349122bc295628d6be9436824613e3b
kul-sudo <ulyanov.kirill@protonmail.com>
2024-04-07 03:51:06 +0300

    add format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,13 +3,17 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 
-		null_ls.setup({
+		null_ls. setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.rustfmt
+				null_ls.builtins.formatting.rustfmt,
 			},
 		})
 
-		vim.keymap.set("n", "zx", vim.lsp.buf.format, {})
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
 	end,
 }
